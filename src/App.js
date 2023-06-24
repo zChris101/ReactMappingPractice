845f8509d6d65f5291aff3ba4051d841bf81013d
zCris101 <chrstnmusico@gmail.com>
2023-06-24 13:01:04 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Entry from "./components/Entry";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      
+      <dl className="dictionary">
+        
+        <Entry />
+
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🙏
+            </span>
+            <span>Person With Folded Hands</span>
+          </dt>
+          <dd>
+            Two hands pressed together. Is currently very introverted, saying a
+            prayer, or hoping for enlightenment. Is also used as a “high five”
+            or to say thank you.
+          </dd>
+        </div>
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              🤣
+            </span>
+            <span>Rolling On The Floor, Laughing</span>
+          </dt>
+          <dd>
+            This is funny! A smiley face, rolling on the floor, laughing. The
+            face is laughing boundlessly. The emoji version of “rofl“. Stands
+            for „rolling on the floor, laughing“.
+          </dd>
+        </div>
+      </dl>
+    </div>
+  );
+}
+
+export default App;
